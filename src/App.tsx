6eeef0b6804d71edf9806b42c79f01f4585fb09f
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2020-08-22 17:54:54 -0300

    Add-text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, StatusBar} from 'react-native';
+import {View, StatusBar, Text} from 'react-native';
 
 const App: React.FC = () => (
     <>
         <StatusBar barStyle="dark-content" backgroundColor="#d6d5c2" />
-        <View style={{flex: 1, backgroundColor: '#d6d5c2'}} />
+        <View style={{flex: 1, backgroundColor: '#d6d5c2'}}>
+            <Text>Enxoval Fácil</Text>
+        </View>
     </>
 );
 
